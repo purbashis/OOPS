866fe5481d38531e778b3649ca448d1f44f9242d
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2022-08-13 21:46:13 +0530

    OOPS
    theory oops concept for interview perpose

---
+++ java_oops/oops_concept.java
@@ -0,0 +1,136 @@
+/*
+
+programming languages can be classified into 3 primary types
+
+Unstructured Programming Languages: 
+The most primitive of all programming languages having sequentially flow of control. Code is repeated through out the program
+Structured Programming Languages:
+ Has non-sequentially flow of control. Use of functions allows for re-use of code.
+Object Oriented Programming Languages:
+ Combines Data & Action Together.
+ 
+
+OOPs Concepts in Java:
+ What is OOPS?
+*Object -Oriented Programing System(OOPs) is a programing concept that works on the principal of abstraction,polymerphism,inheritance,and encapsulation.
+
+*it allows  users to create objects they want and create methods to handle thoes objects.
+
+
+
+OOPs Concepts in Java with Examples
+1)Class :
+The class is the one of theBasic concept of OOPs 
+which is a group of similar entities.
+It is only a logical component not the physical entity.
+
+
+example:
+If you had a class called " Expensive Cars" it could have Objects like  BMW,jaguar,Mercedes,TOYOTAetc.
+ 
+Its properties can be Price or Speed of these cars.
+While the methods may be performed with these cars are driving ,braking,reverse etc.
+
+
+2)Object:
+An object can be defined as an instance of this class,and there can be multiple instances of the class in a program.
+
+An Object is one of the java OOPS concepts which contain both the data and the function ,which operates on the data.
+
+
+example:
+chair,bike,table,car ,etc.
+
+
+3)Inheritance:
+Inheritance is one of the Basic Concepts of OOPs in
+which one object acquires the properties and behaviors of the parent object.
+
+Its creating a parent--child relationship between two classes.
+
+4)Polymerphism:
+Polymorphism means "many forms".
+
+Polymorphism refers to one of the OOPS concept in java which is the ability of 
+a variable,object or function to take on multiple forms.
+
+
+Example:
+In english ,the verb "run "
+has  different meaning if you use with a laptop,a foot race and a business.
+
+Here ,we understand the meening of "run" based on the other words used along with it.
+
+It same also applied to Polymorphism.
+
+5)Abstraction:
+Abstraction is one of the OOp concept in java which is an act of representing essential features 
+without including background details.
+
+it is a technique of creating a new data type that is suited for a specific application. 
+
+
+example:
+While driving a car you don not have to be concerned with its internal working.
+here you just need to be concerned with the parts like gears,steering wheel,accelerator,etc.
+
+6)Encapsulation:
+Encapsulation is one of the best OOps Concept of Wrapping the data and code.
+In this OOps Concept the variables of a class are always hidden from other classes.
+
+it can be accessed using the methods of their current class.
+
+example:
+in school ,
+a student cannot exist without a class.
+---------------------------------------------------------------------------------------------------------------------------------
+7)Association:
+Association is a relationship between two objects.
+It is one of the OOP Concepts in Java which defines the diversity between objects.
+In this OOP concept, all objects have their separate lifecycle, and there is no owner.
+
+
+example:
+many students can associate with one teacher while one student can also associate with multiple teachers.
+
+
+8)Aggregation:
+In this technique, all objects have their separate lifecycle.
+ However, there is ownership such that child object can’t belong to another parent object.
+  For example consider class/objects department and teacher. Here, a single teacher can’t belong to multiple departments, 
+  but even if we delete the department, the teacher object will never be destroyed.
+
+
+  9) Composition
+Composition is a specialized form of Aggregation. It is also called “death” relationship. 
+Child objects do not have their lifecycle so when parent object deletes all child object will also delete automatically. 
+For that, let’s take an example of House and rooms. Any house can have several rooms. One room can’t become part of two different houses.
+ So, if you delete the house  -  room will also be deleted.
+
+
+Advantages of OOPs (Object-Oriented Programming System):
+*Reuse of code through inheritance
+*Flexibility through polymorphism
+*Effective problem solving
+*Modularity for easier troubleshooting.and easy to understand.
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+ */
